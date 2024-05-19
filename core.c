3b51efa95353facb66dd1d534479c49368cde1a5
Mok <mok@moekm.com>
2024-05-19 18:36:14 +0300

    refactoring + new bot

--- core.c
+++ core.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
 /**
  * Simple Tic Tac Toe game using the MiniMax Algorithm 
  * TODO:
+ * - Implement the miniMax algo
+ * - Add configuration options (custom symbols - debug display - etc..)
  * - Add row column number support A1 - B3 - C2
- * - Add an option to let players choose their symbols
- * - Add GUI Support
- * - Add Custom Game Modes (EASY - NORMAL - HARD [aka. minimax])
+ * - Add GUI Support?
 */
 
-void update(int grid[], char symbols[]);            // updates the grid
-int status(int grid[], bool *stop);                 // checks for wins 
-int selected(int grid[], int input);                // checks if the square is already selected
-void plyturn(int num, int *player);                 // calcuates the player turns from the round
-
-enum MSGID { SELECTED , NUMBERS_ONLY , WIN , DRAW , UNKOWN_ERR};
-
-int main() {
-
-    /**
+   /**
      * Drawing the grid in the terminal (NO GUI SUPPORT YET)
      *     1   2   3
      * A | O | X | O |    
@@ -26,61 +20,98 @@ int main() {
      * C | X | O | X |
     */
 
+void update();                         // updates the grid
+int status(bool *stop);                // checks for wins 
+bool selected(int input);               // checks if the square is already selected
+void plyturn(int round, int *player);  // calcuates the player turns from the round
+void computerPlay(int mode, int player[]);  // will assain what bot to play against
+void randoBot();                       // a bot that you can play against
+
+
+enum MSGID { SELECTED , NUMBERS_ONLY, SELECT_MODE, INVALID_MODE, WIN , DRAW};
+enum MODES { Human = 1 , EasyBot, MiniMax };
+enum PLAYERS_SYMBOLS_GRID_ID { X = -1 , O = 1};
+
+
+   // Globals
    int  grid[9] = {0,0,0,0,0,0,0,0,0};
-   int  input, round, winner;
-   int player[2]; // {playerID, playerDisplayNumber}
    char symbols[2] = {'X', 'O'};
-   char MSG[][100] = {
-    "\nThe square has already been selected :(",
-    "\nvalid inputs are between 1-9",
+
+    char MSG[][100] = {
+    "\nThe square has already been selected.",
+    "\nValid inputs are between 1 to 9",
+    "\n1- Offline [2 Players]\n2- Vs Bot 1 [Easy]\n3- Vs MiniMax [Hard]\nSelect Mode: ",
+    "\nMode Not Found! Please Select (1 - 2 - 3)",
     "\nYay! %c HAS WON!",
-    "\nDRAW! Guess you're both stuck in the \"Suck Zone\" >:)",
-    "\nUnable to load the game :O" // no display yet
+    "\nDRAW! Maybe try using both hands?"
    };
 
+int main() {
 
-    // initial -> grid passed as a pointer
-    update(grid, symbols); 
+   int input, mode, player[2]; // player = {playerID (for the grid), playerDisplayNumber}
+   int round, winner;
+   bool stop;
 
-    /*
-    * Mode 1: Local/Offline
-    */
+
+   // Select Play Mode
+   while(true){
+    printf("%s", MSG[SELECT_MODE]);
+
+    if (scanf("%d", &input) != 1){
+     getchar(); // consume '\n'
+     printf("%s\n", MSG[INVALID_MODE]);
+     continue;
+    }
+
+    if (input > 3  || input <= 0){
+     printf("%s\n", MSG[INVALID_MODE]);
+     continue;
+    }
+
+    mode = input;
+
+    break;
+    }
+
+    // initialize grid
+    update(); 
 
     while (round < 9) {
-
-        bool redo = true; 
-        bool stop = false;
+        stop = false;
+        plyturn(round, player); // check who's turn it is
        
-        do {
+        while (true) {
             
-            plyturn(round, player); // check whose turn it is
+            if (mode != Human && player[1] == 2){ // will play as player 2
+                computerPlay(mode, player);       // Reminder: Change this later to make it more dynamic :O
+                update();
+                break;
+            } 
+
             printf("\nPlayer (%d):", player[1]);
 
             if (scanf("%d", &input) != 1){
             getchar(); // consume '\n'
             printf(MSG[NUMBERS_ONLY]);
             continue;
+            } else if (input > 9 || input <= 0){
+                printf(MSG[NUMBERS_ONLY]);
+                continue;
             }
 
-            // check for allowed numbers 1-9
-            if (input > 9 || input <= 0){
-                printf(MSG[NUMBERS_ONLY]);
+            if (selected(input)){
+                update();
+                printf("\n%s", MSG[SELECTED]);
                 continue;
             } 
 
-            if (selected(grid, input)){
-                redo = true;
-                update(grid, symbols);
-                printf("\n%s", MSG[SELECTED]);
-            } else {
-                redo = false;
+                // update the grid value
                 grid[input - 1] = player[0]; 
-                update(grid, symbols);
-            }
+                update();
+                break;
+        }
 
-        } while (redo);
-
-        winner = status(grid, &stop);
+        winner = status(&stop);
         if (stop) break; 
 
         // printf("\nround: %d", round);
@@ -94,7 +125,7 @@ int main() {
     return 0;
 }
 
-void update(int grid[], char symbols[]){
+void update(){
 
     // clears previous logs
     printf("\033[H\033[J");
@@ -130,13 +161,13 @@ void update(int grid[], char symbols[]){
 
 }
 
-int selected(int grid[], int input){
+bool selected(int input){
     if (grid[input - 1] == -1 || grid[input - 1] == 1){
-        return 1; // true
+        return true; // true
     }
 }
 
-int status(int grid[], bool *stop){
+int status(bool *stop){
 
     int player;
 
@@ -169,16 +200,43 @@ int status(int grid[], bool *stop){
     if (*stop) return player;
 }
 
-void plyturn(int num, int *player){
+void plyturn(int round, int *player){
 
-    num = num % 2;
-    if (num == 1){
+    round %= 2;
+    if (round == 1){
         *(player) = 1; // O
         *(player + 1) = 2; 
     } 
     
-      if (num == 0){
+      if (round == 0){
         *(player) = -1; // X
         *(player + 1) = 1; 
     } 
+}
+
+void computerPlay(int mode, int player[]){
+
+switch (mode) {
+    case EasyBot: // easy
+    randoBot();
+    break;
+
+    case MiniMax: // minimax (will break, for now o:)
+          printf("\t\t\t\nminimax is unavailable now\n\t\t\t");
+    break;
+}
+}
+
+void randoBot(){
+
+     int botPosition;
+     srand(time(0)); // seed
+    
+        do {
+        botPosition = rand() % 10; // upto 9
+        }
+        while(selected(botPosition));
+
+        grid[botPosition - 1] = 1;
+       
 }
